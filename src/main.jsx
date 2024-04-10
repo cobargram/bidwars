ff1cfa3d6086e59bab9aa66c9307b5bb2f0f2474
Joshua Oyebode <oyebodeb@gmail.com>
2024-04-10 16:40:57 +0100

    navbar not showing fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "../node_modules/jquery/dist/jquery.min.js";
+//import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons-font/dist/bootstrap-icons-font.min.css"
 
 const appRoot = document.getElementById('root');
 
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/navbar.jsx";
 
 function Layout(){
     return (
         <>
-            <div>Layout container</div>
+            <Navbar />
             <main>
                 <Outlet />
             </main>
