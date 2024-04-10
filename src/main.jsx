44cd23658734949edf3ec0048c9da74bf585ea4d
solzycoded <solomonfidelis012@gmail.com>
2024-04-10 16:59:00 +0100

    changed 'class' to 'className' in the navbar component and removed cdn bootstrap references from index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
 import "../node_modules/jquery/dist/jquery.min.js";
 //import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
